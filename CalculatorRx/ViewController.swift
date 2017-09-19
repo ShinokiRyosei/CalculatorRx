559aef536afe608fa913cd6de714e9e8b809677a
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-09-20 01:45:10 +0900

    IBOutlet

--- CalculatorRx/ViewController.swift
+++ CalculatorRx/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+
+    @IBOutlet weak var answerLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
